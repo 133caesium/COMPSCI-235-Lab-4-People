a0d775cf1cfcf4bc6ec8e385167f9cad102758a9
nber106 <nber106@aucklanduni.ac.nz>
2021-08-22 14:28:33 +1200

    connected .env and config.py to flask app through __init__.py

--- people_web_app/__init__.py
+++ people_web_app/__init__.py
@@ -7,6 +7,9 @@ from people_web_app.domain.model import Person
 def create_app():
     app = Flask(__name__)
 
+    # Configure the app from configuration-file settings.
+    app.config.from_object('config.Config')
+
     repo.repo_instance = repo.PeopleRepository(
         Person(74633, 'Julius', 'Caeser'),
         Person(88337, 'Genghis', 'Khan'),
