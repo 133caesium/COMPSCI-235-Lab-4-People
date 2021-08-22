39d77a730306332b23ab9ad92634a9bd540e85d2
nber106 <nber106@aucklanduni.ac.nz>
2021-08-22 14:48:17 +1200

    added the blueprint to flask app

--- people_web_app/__init__.py
+++ people_web_app/__init__.py
@@ -18,4 +18,8 @@ def create_app():
         Person(92213, 'Nelson', 'Mandela')
     )
 
+    with app.app_context():
+        from .people_blueprint import people
+        app.register_blueprint(people.people_blueprint)
+
     return app
